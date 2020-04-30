48675f12904a66c859efd052c76992fd052f346a
mrarthurwhite <mister.arthur.white@gmail.com>
2020-04-30 13:27:51 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyeOnMe extends Component {
+
+  constructor() {
+    super()
+  }
+
+
+  compliment = () => {
+    console.log('Good!')
+  }
+
+
+  print = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  render() {
+
+    return (
+      <button onFocus={this.compliment} onBlur={this.print}>Enter</button>
+    )
+  }
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+  constructor() {
+    super();
+
+  }
+
+  print = () => {
+    console.log('Entering password...');
+  }
+
+  render() {
+
+    return (
+        <input type="password" onKeyUp={this.print}/>
+    )
+  }
+
+}
